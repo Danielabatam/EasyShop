8a2bcbcc8a09d2ba944d709e8de45ee50bcb75ca
danielabatam <danielabatam@gmail.com>
2023-06-09 19:59:35 +0100

    [FEAT]: App.js setting routes

--- esayshop/src/App.js
+++ esayshop/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import Header from "./common/header/Header";
+// import {BrowserRouter as Routes, Route} from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      // <Routes>
+      //     <Route path={"/"} element={<Home/>}></Route>
+
+    // <BrowserRouter>
+    // <BrowserRouter>
+    //   <Header/>
+    //     <Route>
+    //         <Route path={"/"} exact><Header/></Route>
+    //     </Route>
+    // </BrowserRouter>
+
+      <Router>
+          <Header/>
+          <Switch>
+              {/*<Route path={"/"} exact><Header/></Route>*/}
+          </Switch>
+      </Router>
+  )
 }
 
-export default App;
+export default App
